085fa43f15800f417d3837578b8ee37804b38120
GreymanGamingLabs <greymangaminglabs@gmail.com>
2026-07-23 11:50:26 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,123 @@
+const questions = [
+
+{
+id:1,
+question:"Which mob gives wool?",
+answer:"Sheep"
+},
+
+{
+id:2,
+question:"What block is needed to build a Nether Portal frame?",
+answer:"Obsidian"
+},
+
+{
+id:3,
+question:"Which mob explodes when it gets close?",
+answer:"Creeper"
+},
+
+{
+id:4,
+question:"What do villagers use as currency?",
+answer:"Emeralds"
+},
+
+{
+id:5,
+question:"Which mob drops Ender Pearls?",
+answer:"Endermen"
+},
+
+{
+id:6,
+question:"What do bees produce?",
+answer:"Honey"
+},
+
+{
+id:7,
+question:"Which ore is needed to craft a diamond pickaxe?",
+answer:"Diamonds"
+},
+
+{
+id:8,
+question:"Which dimension is home to the Ender Dragon?",
+answer:"The End"
+},
+
+{
+id:9,
+question:"What tool is best for mining stone?",
+answer:"Pickaxe"
+},
+
+{
+id:10,
+question:"Which animal can be tamed using bones?",
+answer:"Wolf"
+},
+
+{
+id:11,
+question:"What food do cows eat?",
+answer:"Wheat"
+},
+
+{
+id:12,
+question:"Which block lets players sleep through the night?",
+answer:"Bed"
+},
+
+{
+id:13,
+question:"Which mob shoots arrows?",
+answer:"Skeleton"
+},
+
+{
+id:14,
+question:"What do chickens lay?",
+answer:"Eggs"
+},
+
+{
+id:15,
+question:"Which ore gives Redstone Dust?",
+answer:"Redstone Ore"
+},
+
+{
+id:16,
+question:"What is the strongest naturally generated block?",
+answer:"Bedrock"
+},
+
+{
+id:17,
+question:"Which item allows players to glide?",
+answer:"Elytra"
+},
+
+{
+id:18,
+question:"Which structure contains Blaze spawners?",
+answer:"Nether Fortress"
+},
+
+{
+id:19,
+question:"Which biome is mostly made of sand?",
+answer:"Desert"
+},
+
+{
+id:20,
+question:"Who created Minecraft?",
+answer:"Markus 'Notch' Persson"
+}
+
+];
